6de5dd43d020f5c4849d12ea2cd6ac9e5f547fc2
karthik2628 <karthikkishore2603@gmail.com>
2022-09-21 22:30:45 +0530

    commit5

--- app/__init__.py
+++ app/__init__.py
@@ -1,10 +1,10 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-from flask_ngrok import run_with_ngrok
+
 
 
 app = Flask(__name__)
-run_with_ngrok(app)
+
 
 SQLALCHEMY_DATABASE_URL = "mysql://karthik:password@localhost:3306/service"
 
